cf0dc0a5416bedcd0f1424a452e50d9d2302f30f
Derek Hansen <derek@kaha.us>
2020-06-06 19:47:24 -0600

    unused dependency

--- moviesapp/movies/models.py
+++ moviesapp/movies/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse
-from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MaxValueValidator
